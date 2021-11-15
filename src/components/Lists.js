67a25e0672f409d3840ffbd27f9625b3ddb995ed
Fernando Serpa <serpaef@outlook.com>
2021-11-15 03:37:30 -0300

    impl: efeitos css nas listas de filmes

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './Lists.css';
 
-function Lists({Movies}) {
+function Lists({ Movies }) {
   return (
     <section className="lists">
-      {Movies && Movies.map(({title, items: {results}}, index) => (
-        <div>
-          <h2>{title}</h2>
-          <div className="listRow">
-            {results.length && results.map((movie, index) => (
-              <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.original_name}/>
-            )) }
+      { Movies && Movies.map(({ title, items: { results } }, index) => (
+        <div className="listCarrousel" key={index}>
+          <h2>{ title }</h2>
+          <div className="listArea">
+            <div className="listRow">
+              { results.length && results.map((movie, index) => (
+                <div className="movieCard" key={index}>
+                  <img src={ `https://image.tmdb.org/t/p/w200${ movie.poster_path }` } alt={ movie.original_name } />
+                </div>
+              )) }
+            </div>
           </div>
         </div>
-      ))}
+      )) }
     </section>
   )
 }
