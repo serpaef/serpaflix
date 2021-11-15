8d2c2d0bf778ab083b6f9235a15cbb59c8334626
Fernando Serpa <serpaef@outlook.com>
2021-11-15 00:46:47 -0300

    primeira versao da pagina de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+    <Routes>
+      <Route exact path="/" element={ <LoginPage/> } />
+      <Route path="/home" element={ <Home /> } />
+    </Routes>
+  )
 }
 
 export default App;
