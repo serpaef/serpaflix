a9b74b704d30df8afc30db79357a66c63eae1dbe
Fernando Serpa <serpaef@outlook.com>
2021-11-15 03:16:22 -0300

    impl: primeiros passos cards + css

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Lists.css';
 
 function Lists({Movies}) {
   return (
     <section className="lists">
-      {Movies && Movies.map((item, index) => (
+      {Movies && Movies.map(({title, items: {results}}, index) => (
         <div>
-          {item.title}
+          <h2>{title}</h2>
+          <div className="listRow">
+            {results.length && results.map((movie, index) => (
+              <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.original_name}/>
+            )) }
+          </div>
         </div>
       ))}
     </section>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import getLists from '../api/tmdb'
-import Header from '../components/Header'
+// import Header from '../components/Header'
 import Lists from '../components/Lists';
 
 function Home() {
