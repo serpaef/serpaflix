90acab276b4779c4c6dcc4e86e3869684a538f12
Fernando Serpa <serpaef@outlook.com>
2021-11-16 02:38:12 -0300

    impl: logica dos botoes de rolar carrossel (TODO: Resolver bug de rolar todos os carroseis)

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Lists.css';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 function Lists({ Movies }) {
+  const [position, setPosition] = useState(-500);
+
+  const handleLeft = () => {
+    let pos = position + 150;
+    if (pos > 0) pos = 0;
+    setPosition(pos);
+  }
+
+  const handleRight = () => {
+    let pos = position - 150;
+    setPosition(pos);
+  }
+
   return (
     <section className="lists">
       { Movies && Movies.map(({ title, items: { results } }, index) => (
         <div className="listCarrousel" key={index}>
           <h2>{ title }</h2>
           <div className="listLeft">
-            <ChevronLeftIcon style={{fontSize: 40}} />
+            <ChevronLeftIcon style={{fontSize: 40}} onClick={ handleLeft }/>
           </div>
           <div className="listRight">
-            <ChevronRightIcon style={{fontSize: 40}} />
+            <ChevronRightIcon style={{fontSize: 40}} onClick={ handleRight }/>
           </div>
           <div className="listArea">
-            <div className="listRow">
+            <div className="listRow" style={{width: 'max-content', marginLeft: position}}>
               { results.length && results.map((movie, index) => (
                 <div className="movieCard" key={index}>
                   <img src={ `https://image.tmdb.org/t/p/w200${ movie.poster_path }` } alt={ movie.original_name } />
